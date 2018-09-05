98211bd18e4a6bd27dbeba25abf4badc484a86cc
github username <github email address>
2018-09-04 23:20:25 -0400

    further practice with object relationships

--- author.rb
+++ author.rb
@@ -1,1 +1,46 @@
+class Author
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def books
+    Book.all.select do |book_obj|
+      book_obj.author == self
+    end
+  end
+
+  def write_book(title, word_count)
+    Book.new(self, title, word_count)
+  end
+
+  def total_words
+    word_array = self.books.map do |book_obj|
+      book_obj.word_count
+    end
+    word_array.inject(0) do |sum, x|
+      sum + x
+    end
+  end
+
+  def self.most_words
+    word_array = self.all.map do |author_obj|
+      author_obj.total_words
+    end.sort
+    self.all.select do |author_obj|
+       if author_obj.total_words == word_array[-1]
+         author_obj
+       end
+    end
+  end
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,12 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+a1 = Author.new("Bob")
+a2 = Author.new("Anne")
+a3 = Author.new("Emily")
+b1 = Book.new(a1, "Book 1", 100)
+b2 = Book.new(a1, "Book 2", 350)
+b3 = Book.new(a2, "Book 3", 400)
+b4 = Book.new(a3, "Book 4", 200)
+
 binding.pry
-
-
